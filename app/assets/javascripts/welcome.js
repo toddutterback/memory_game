cd313d6658e6fee1c6bc0f4478cc8119cf6bf188
Todd Utterback <todd.j.utterback@gmail.com>
2016-03-17 09:32:53 -0600

    initial commit

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+  var userChoice
+
+
+  $('.grid_box').click(function() {
+    $(this).toggleClass('blue');
+  });
+
+
+ Memory.prototype.options = {
+    wrapperID : "container",
+    cards : [
+      {
+        id : 1,
+        img: "images/Baylor.png"
+      },
+      {
+        id : 2,
+        img: "images/Butler.png"
+      },
+      {
+        id : 3,
+        img: "images/CUBoulder.jpeg"
+      },
+      {
+        id : 4,
+        img: "images/Duke.jpg"
+      },
+      {
+        id : 5,
+        img: "images/Gonzaga.png"
+      },
+      {
+        id : 6,
+        img: "images/Iowa.jpeg"
+      },
+      {
+        id : 7,
+        img: "images/MSUpng"
+      },
+      {
+        id : 8,
+        img: "images/Oregon.jpeg"
+      },
+      {
+        id : 9,
+        img: "images/the_rock..jpg"
+      },
+      {
+        id : 10,
+        img: "images/UNC.jpg"
+      },
+      {
+        id : 11,
+        img: "images/UofA.jpeg"
+      },
+      {
+        id : 12,
+        img: "images/UofA.png"
+      },
+      {
+        id : 13,
+        img: "images/UTAustin.jpg"
+      },
+      {
+        id : 14,
+        img: "images/UVA.jpeg"
+      },
+      {
+        id : 15,
+        img: "images/VCU.png"
+      },
+      {
+        id : 16,
+        img: "images/Yale.png"
+      }
+    ]}
+
+
+
+});
+
